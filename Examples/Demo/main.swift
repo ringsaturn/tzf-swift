dba90f6e287b581a68db14656aa8a89cae4b1ac4
Han Xiao <ringsaturn.me@gmail.com>
2026-04-08 21:39:04 +0900

    fmt

--- Examples/Demo/main.swift
+++ Examples/Demo/main.swift
@@ -2,21 +2,21 @@ import Foundation
 import tzf
 
 do {
-    let finder = try DefaultFinder()
+  let finder = try DefaultFinder()
 
-    let timezone = try finder.getTimezone(lng: 116.3833, lat: 39.9167)
-    print("Beijing timezone:", timezone)
+  let timezone = try finder.getTimezone(lng: 116.3833, lat: 39.9167)
+  print("Beijing timezone:", timezone)
 
-    let timezones = try finder.getTimezones(lng: 87.5703, lat: 43.8146)
-    print("Multiple possible timezones:", timezones)
+  let timezones = try finder.getTimezones(lng: 87.5703, lat: 43.8146)
+  print("Multiple possible timezones:", timezones)
 
-    print("Data version:", finder.dataVersion())
+  print("Data version:", finder.dataVersion())
 
-    if let macauGeoJSON = finder.getTimezoneGeoJSON(timezoneName: "Asia/Macau") {
-        print("Asia/Macau features:", macauGeoJSON.features.count)
-        print(try macauGeoJSON.toJSONString(pretty: false))
-    }
+  if let macauGeoJSON = finder.getTimezoneGeoJSON(timezoneName: "Asia/Macau") {
+    print("Asia/Macau features:", macauGeoJSON.features.count)
+    print(try macauGeoJSON.toJSONString(pretty: false))
+  }
 } catch {
-    print("Error:", error)
-    exit(1)
+  print("Error:", error)
+  exit(1)
 }
